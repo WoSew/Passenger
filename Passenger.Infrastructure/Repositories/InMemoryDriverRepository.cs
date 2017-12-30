20ce11444d217ac7d6a0867a5400f8a3d5731cd5
WoSew <wojseweryn@gmail.com>
2017-12-30 18:45:18 +0100

    2 simple unit tests

--- Passenger.Infrastructure/Repositories/InMemoryDriverRepository.cs
+++ Passenger.Infrastructure/Repositories/InMemoryDriverRepository.cs
@@ -9,8 +9,8 @@ namespace Passenger.Infrastructure.Repositories
 {
     public class InMemoryDriverRepository : IDriverRepository
     {
-
         private static ISet<Driver> _drivers = new HashSet<Driver>();
+
         public async Task<Driver> GetAsync(Guid userId)
             =>await Task.FromResult(_drivers.Single(x=> x.UserId == userId));
 
@@ -22,12 +22,14 @@ namespace Passenger.Infrastructure.Repositories
             _drivers.Add(driver);
             await Task.CompletedTask;
         }
+
         public async Task RemoveAsync(Guid userId)
         {
             var driver = await GetAsync(userId);
             _drivers.Remove(driver);
             await Task.CompletedTask;
         }
+
         public async Task UpdateAsync(Driver driver)
         {
             await Task.CompletedTask;
--- Passenger.Tests/Services/UserServiceTests.cs
+++ Passenger.Tests/Services/UserServiceTests.cs
@@ -22,7 +22,39 @@ namespace Passenger.Tests.Services
             await userService.RegisterAsync("user@email.com", "user", "secret");
 
             userRepositoryMock.Verify(x => x.AddAsync(It.IsAny<User>()), Times.Once);
-            
+        }
+
+        [Fact]
+        public async Task when_calling_get_async_and_user_exist_it_should_invoke_user_repository_get_async()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var mapperMock = new Mock<IMapper>();
+
+            var userService = new UserService(userRepositoryMock.Object, mapperMock.Object);
+
+            await userService.GetAsync("user1@email.com");
+
+            var user = new User("user1@email.com", "user1", "secret", "salt");
+
+            userRepositoryMock.Setup( x => x.GetAsync(It.IsAny<string>()))
+                              .ReturnsAsync(user);
+            userRepositoryMock.Verify( x=> x.GetAsync(It.IsAny<string>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task when_calling_get_async_and_user_does_not_exist_it_should_invoke_user_repository_get_async()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var mapperMock = new Mock<IMapper>();
+
+            var userService = new UserService(userRepositoryMock.Object, mapperMock.Object);
+
+            await userService.GetAsync("userNULL@email.com");
+
+            userRepositoryMock.Setup( x=> x.GetAsync(It.IsAny<string>()))
+                              .ReturnsAsync( () => null);
+                              
+            userRepositoryMock.Verify( x=> x.GetAsync(It.IsAny<string>()), Times.Once());
         }
         
     }
