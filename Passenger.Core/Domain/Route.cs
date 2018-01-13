f1663f8ad4d77ba9455dafb78dc64dcb82f75c79
WoSew <wojseweryn@gmail.com>
2018-01-13 19:08:02 +0100

    expanding the class Route

--- Passenger.Core/Domain/Route.cs
+++ Passenger.Core/Domain/Route.cs
@@ -7,5 +7,17 @@ namespace Passenger.Core.Domain
         public Guid Id { get; protected set; }
         public Node StartNode { get; protected set; }
         public Node EndNode { get; protected set; }
+
+        protected Route(Node startNode, Node endNode)
+        {
+            Id = Guid.NewGuid();
+            StartNode = startNode;
+            EndNode = endNode;
+            
+        }
+
+        public static Route Create(Node startNode, Node endNode)
+            => new Route(startNode, endNode);
+        
     }
 }
